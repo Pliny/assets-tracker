a075db7964fab19bdf60e678bbb433229184dbda
Dave Desrochers <dave726@gmail.com>
2013-11-24 17:34:58 -0800

    Added boring but functional home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AssetTracker::Application.routes.draw do
+  root 'assets#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
