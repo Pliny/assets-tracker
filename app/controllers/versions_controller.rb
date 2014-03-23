8781f7a21a59b699e0909dcb4551725c471e9deb
Dave Desrochers <dave726@gmail.com>
2014-03-23 15:10:17 -0700

    Refactored versions rendering
    Version object now has its own partial

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -3,6 +3,6 @@ class VersionsController < ApplicationController
   before_action :authenticate
 
   def index
-    @history = PaperTrail::Version.order("created_at DESC")
+    @versions = PaperTrail::Version.order("created_at DESC")
   end
 end
