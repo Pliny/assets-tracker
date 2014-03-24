de4595740b90da09e96657a4b895454490175c97
Dave Desrochers <dave726@gmail.com>
2014-03-23 19:40:07 -0700

    Added user image to views

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -3,5 +3,9 @@ module AssetsHelper
   def display_location in_house
     in_house == true ? "In House" : "Unknown"
   end
+
+  def get_image_from user
+    user.google_image_url || "default_user_image.png"
+  end
 end
 
